055a125b8c8dd46c53c3932a7e46b63c72f6abd9
nadim <nadim.sheikh.07@gmail.com>
2021-08-21 22:35:33 +0530

    add menu option

--- component/menu/index.js
+++ component/menu/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import List from '@material-ui/core/List';
 import { useStyles } from './styles'
 import MenuItem from './item';
 import MenuHeader from './header';
-import { Fab } from '@material-ui/core';
+import { Fab, List, ListItem, ListItemText, Popover } from '@material-ui/core';
 
 
 const ResMenu = ({ menuData }) => {
     const classes = useStyles();
+    const itemRefs = React.useRef([]);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+
+    const scrollTo = (id) => {
+        if (itemRefs.current) {
+            itemRefs.current[id].scrollIntoView();
+        }
+        handleClose()
+    }
+
     return (
         <div className={classes.root}>
+
             <List className={classes.list} component="nav" subheader={<li />}>
                 {menuData && menuData.map((menu) => (
-                    <li key={`list-${menu.id}`} className={classes.listItem}>
+                    <li key={`list-${menu.id}`} className={classes.listItem} ref={el => (itemRefs.current[menu.id] = el)}>
                         <ul className={classes.ul}>
                             <MenuHeader data={menu} />
                             {menu?.items.map((item) => (
@@ -23,9 +45,33 @@ const ResMenu = ({ menuData }) => {
                 ))}
             </List>
 
-            {/* <Fab color="primary" aria-label="add" className={classes.menuBtn}>
+            <Fab color="primary" aria-label="add" className={classes.menuBtn} onClick={handleClick}>
                 Menu
-            </Fab> */}
+            </Fab>
+
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+
+                <List>
+                    {menuData && menuData.map((menu) => (
+                        <ListItem key={`list-${menu.id}`} onClick={() => scrollTo(menu.id)}>
+                            <ListItemText primary={menu.name} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Popover>
         </div>
     );
 }
