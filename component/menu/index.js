b03e9308c5beef4b1a3cb7f0a3bb70bd08408f1c
nadim <nadim.sheikh.07@gmail.com>
2021-08-21 21:57:20 +0530

    add menu

--- component/menu/index.js
+++ component/menu/index.js
@@ -3,22 +3,30 @@ import List from '@material-ui/core/List';
 import { useStyles } from './styles'
 import MenuItem from './item';
 import MenuHeader from './header';
+import { Fab } from '@material-ui/core';
+
 
 const ResMenu = ({ menuData }) => {
     const classes = useStyles();
     return (
-        <List className={classes.root} component="nav" subheader={<li />}>
-            {menuData && menuData.map((menu) => (
-                <li key={`list-${menu.id}`} className={classes.listSection}>
-                    <ul className={classes.ul}>
-                        <MenuHeader data={menu} />
-                        {menu?.items.map((item) => (
-                            <MenuItem key={`list-item-${menu.id}-${item.id}`} data={item} />
-                        ))}
-                    </ul>
-                </li>
-            ))}
-        </List>
+        <div className={classes.root}>
+            <List className={classes.list} component="nav" subheader={<li />}>
+                {menuData && menuData.map((menu) => (
+                    <li key={`list-${menu.id}`} className={classes.listItem}>
+                        <ul className={classes.ul}>
+                            <MenuHeader data={menu} />
+                            {menu?.items.map((item) => (
+                                <MenuItem key={`list-item-${menu.id}-${item.id}`} data={item} />
+                            ))}
+                        </ul>
+                    </li>
+                ))}
+            </List>
+
+            {/* <Fab color="primary" aria-label="add" className={classes.menuBtn}>
+                Menu
+            </Fab> */}
+        </div>
     );
 }
 
--- component/menu/styles.js
+++ component/menu/styles.js
@@ -2,16 +2,27 @@ import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
     root: {
-        width: '100%',        
-        backgroundColor: theme.palette.background.paper,        
+        margin: 0
+    },
+    list: {
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
         overflow: 'auto',
         maxHeight: 600,
     },
-    listSection: {
+    listItem: {
         backgroundColor: 'inherit',
     },
     ul: {
         backgroundColor: 'inherit',
         padding: 0,
+    },
+    menuBtn: {
+        position: 'absolute',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+        '&:hover': {
+            backgroundColor: theme.palette.secondary,
+        },
     },
 }));
